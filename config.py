5cda80e788df90e5be1b4b041c59abffee304e0e
DH-ESP-MH <91718069+DH-ESP-MH@users.noreply.github.com>
2025-04-11 06:18:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import logging
 import os
+import json  # Для обробки списку ADMIN_IDS
 
-BOT_TOKEN = "8178011882:AAGgVrr68rmXAqJpIdCthC6CojqHaTdTNI0" 
-ADMIN_IDS = [7123063710]  
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+ADMIN_IDS_STR = os.environ.get("ADMIN_IDS")
+ADMIN_IDS = json.loads(ADMIN_IDS_STR) if ADMIN_IDS_STR else []
 DB_PATH = "stationery_shop.db"
 
 logger = logging.getLogger('StationeryBot')
@@ -20,3 +22,10 @@ console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
+
+# Перевірка наявності токена
+if not BOT_TOKEN:
+    logger.error("Змінна середовища BOT_TOKEN не встановлена!")
+    exit()
+
+logger.info(f"Бот запущено з ID адміністраторів: {ADMIN_IDS}")
